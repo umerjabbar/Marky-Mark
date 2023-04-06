b83f9846365b01580a02188125fa2800c0ea9ee1
Umer Jabbar <umerabduljabbar@icloud.com>
2023-04-06 14:12:09 +0500

    pod removed

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "Marky-Mark",
     platforms: [
-        .iOS("8.0"),
+        .iOS(.v12),
     ],
     products: [
         .library(
