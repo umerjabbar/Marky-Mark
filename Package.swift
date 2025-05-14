29889076c66e2a091f6dde47d22f106f2f95916b
Umer Jabbar <umerabduljabbar@icloud.com>
2025-05-14 17:54:44 +0500

    update minimum ios version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.6
 
 import PackageDescription
 
 let package = Package(
     name: "Marky-Mark",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v14),
     ],
     products: [
         .library(
